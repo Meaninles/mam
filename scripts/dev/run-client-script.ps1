59c0fa358f8cdc964a35269ac10a6bd1bc13bda5
Meaninles <jiangxiangkejinb@163.com>
2026-04-08 22:04:07 +0800

    客户端启动命令修改

---
+++ scripts/dev/run-client-script.ps1
@@ -0,0 +1,22 @@
+param(
+  [Parameter(Mandatory = $true, Position = 0)]
+  [string]$ScriptName,
+
+  [Parameter(ValueFromRemainingArguments = $true)]
+  [string[]]$ScriptArgs
+)
+
+$ErrorActionPreference = "Stop"
+
+$clientDir = Resolve-Path (Join-Path $PSScriptRoot "..\..\client")
+
+Push-Location $clientDir
+
+try {
+  & npm run $ScriptName -- @ScriptArgs
+  if ($LASTEXITCODE -ne 0) {
+    exit $LASTEXITCODE
+  }
+} finally {
+  Pop-Location
+}
